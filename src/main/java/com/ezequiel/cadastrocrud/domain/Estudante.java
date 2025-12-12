a5171c22e827fefb2dec8b334a5158f123d08fad
Ezequiel <ezequielsilvalima01@gmail.com>
2025-12-11 22:11:10 -0300

    commit

---
+++ src/main/java/com/ezequiel/cadastrocrud/domain/Estudante.java
@@ -0,0 +1,40 @@
+package com.ezequiel.cadastrocrud.domain;
+
+public class Estudante {
+    private long id;
+    private String nome;
+    private String sexo;
+    private int idade;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
